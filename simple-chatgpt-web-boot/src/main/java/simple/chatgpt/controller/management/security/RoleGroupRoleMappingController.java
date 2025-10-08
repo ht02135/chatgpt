540c1e0ee2da9d3601d6b4e4f34a34041b48eb8c
Hung En Tsai <ht02135@yahoo.com>
2025-10-08 10:53:05 -0400

    commit

--- simple-chatgpt-web-boot/src/main/java/simple/chatgpt/controller/management/security/RoleGroupRoleMappingController.java
+++ simple-chatgpt-web-boot/src/main/java/simple/chatgpt/controller/management/security/RoleGroupRoleMappingController.java
@@ -37,8 +37,12 @@ public class RoleGroupRoleMappingController implements RoleGroupRoleMappingContr
     // ---------------- CREATE ----------------
     @PostMapping("/add")
     public ResponseEntity<Response<RoleGroupRoleMappingPojo>> insertMapping(@RequestBody RoleGroupRoleMappingPojo mapping) {
-        logger.debug("insertMapping called mapping={}", mapping);
+        logger.debug("insertMapping START");
+        logger.debug("insertMapping mapping={}", mapping);
+
         RoleGroupRoleMappingPojo result = mappingService.insertMapping(ParamWrapper.wrap("mapping", mapping));
+
+        logger.debug("insertMapping return={}", result);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(Response.success("Mapping inserted successfully", result, HttpStatus.CREATED.value()));
     }
@@ -48,10 +52,15 @@ public class RoleGroupRoleMappingController implements RoleGroupRoleMappingContr
             @RequestParam Long roleGroupId,
             @RequestParam Long roleId
     ) {
-        logger.debug("addRoleToGroupIfNotExists called, roleGroupId={}, roleId={}", roleGroupId, roleId);
+        logger.debug("addRoleToGroupIfNotExists START");
+        logger.debug("addRoleToGroupIfNotExists roleGroupId={}", roleGroupId);
+        logger.debug("addRoleToGroupIfNotExists roleId={}", roleId);
+
         RoleGroupRoleMappingPojo result = mappingService.addRoleToGroupIfNotExists(
-        		ParamWrapper.wrap("roleGroupId", roleGroupId, "roleId", roleId)
+                ParamWrapper.wrap("roleGroupId", roleGroupId, "roleId", roleId)
         );
+
+        logger.debug("addRoleToGroupIfNotExists return={}", result);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(Response.success("Role added to role group if not exists successfully", result, HttpStatus.CREATED.value()));
     }
@@ -59,8 +68,12 @@ public class RoleGroupRoleMappingController implements RoleGroupRoleMappingContr
     // ---------------- DELETE ----------------
     @DeleteMapping("/deleteById")
     public ResponseEntity<Response<Void>> deleteMappingById(@RequestParam Long id) {
-        logger.debug("deleteMappingById called, id={}", id);
+        logger.debug("deleteMappingById START");
+        logger.debug("deleteMappingById id={}", id);
+
         mappingService.deleteMappingById(ParamWrapper.wrap("id", id));
+
+        logger.debug("deleteMappingById DONE");
         return ResponseEntity.ok(Response.success("Mapping deleted successfully", null, HttpStatus.OK.value()));
     }
 
@@ -69,69 +82,81 @@ public class RoleGroupRoleMappingController implements RoleGroupRoleMappingContr
             @RequestParam Long roleGroupId,
             @RequestParam Long roleId
     ) {
-        logger.debug("deleteMappingByGroupAndRole called, roleGroupId={}, roleId={}", roleGroupId, roleId);
+        logger.debug("deleteMappingByGroupAndRole START");
+        logger.debug("deleteMappingByGroupAndRole roleGroupId={}", roleGroupId);
+        logger.debug("deleteMappingByGroupAndRole roleId={}", roleId);
+
         mappingService.deleteMappingByGroupAndRole(ParamWrapper.wrap("roleGroupId", roleGroupId, "roleId", roleId));
+
+        logger.debug("deleteMappingByGroupAndRole DONE");
         return ResponseEntity.ok(Response.success("Mapping deleted successfully", null, HttpStatus.OK.value()));
     }
 
     // ---------------- READ ----------------
     @GetMapping("/listAll")
     public ResponseEntity<Response<PagedResult<RoleGroupRoleMappingPojo>>> findAllMappings() {
-        logger.debug("findAllMappings called");
+        logger.debug("findAllMappings START");
+
         PagedResult<RoleGroupRoleMappingPojo> result = mappingService.findAllMappings();
+
+        logger.debug("findAllMappings return={}", result);
         return ResponseEntity.ok(Response.success("Mappings fetched successfully", result, HttpStatus.OK.value()));
     }
 
     @GetMapping("/listByRoleGroup")
     public ResponseEntity<Response<PagedResult<RoleGroupRoleMappingPojo>>> findByRoleGroupId(@RequestParam Long roleGroupId) {
-        logger.debug("findByRoleGroupId called, roleGroupId={}", roleGroupId);
+        logger.debug("findByRoleGroupId START");
+        logger.debug("findByRoleGroupId roleGroupId={}", roleGroupId);
+
         PagedResult<RoleGroupRoleMappingPojo> result = mappingService.findByRoleGroupId(ParamWrapper.wrap("roleGroupId", roleGroupId));
+
+        logger.debug("findByRoleGroupId return={}", result);
         return ResponseEntity.ok(Response.success("Mappings fetched successfully", result, HttpStatus.OK.value()));
     }
 
     @GetMapping("/listByRole")
     public ResponseEntity<Response<PagedResult<RoleGroupRoleMappingPojo>>> findByRoleId(@RequestParam Long roleId) {
-        logger.debug("findByRoleId called, roleId={}", roleId);
+        logger.debug("findByRoleId START");
+        logger.debug("findByRoleId roleId={}", roleId);
+
         PagedResult<RoleGroupRoleMappingPojo> result = mappingService.findByRoleId(ParamWrapper.wrap("roleId", roleId));
+
+        logger.debug("findByRoleId return={}", result);
         return ResponseEntity.ok(Response.success("Mappings fetched successfully", result, HttpStatus.OK.value()));
     }
 
     // ---------------- SEARCH / PAGINATION ----------------
     @GetMapping("/findMappings")
     public ResponseEntity<Response<PagedResult<RoleGroupRoleMappingPojo>>> findMappings(@RequestParam Map<String, Object> params) {
-        logger.debug("findMappings called, params={}", params);
+        logger.debug("findMappings START");
+        logger.debug("findMappings params={}", params);
+
         PagedResult<RoleGroupRoleMappingPojo> result = mappingService.findMappings(params);
+
+        logger.debug("findMappings return={}", result);
         return ResponseEntity.ok(Response.success("Mappings fetched successfully", result, HttpStatus.OK.value()));
     }
 
     @GetMapping("/searchMappings")
     public ResponseEntity<Response<PagedResult<RoleGroupRoleMappingPojo>>> searchMappings(@RequestParam Map<String, Object> params) {
-        logger.debug("searchMappings called, params={}", params);
+        logger.debug("searchMappings START");
+        logger.debug("searchMappings params={}", params);
+
         PagedResult<RoleGroupRoleMappingPojo> result = mappingService.searchMappings(params);
+
+        logger.debug("searchMappings return={}", result);
         return ResponseEntity.ok(Response.success("Mappings fetched successfully", result, HttpStatus.OK.value()));
     }
 
     // ---------------- COUNT ----------------
     @GetMapping("/countMappings")
     public ResponseEntity<Response<Long>> countMappings(@RequestParam Map<String, Object> params) {
-        logger.debug("countMappings called, params={}", params);
+        logger.debug("countMappings START");
+        logger.debug("countMappings params={}", params);
+
         long count = mappingService.countMappings(params);
+
+        logger.debug("countMappings return={}", count);
         return ResponseEntity.ok(Response.success("Count fetched successfully", count, HttpStatus.OK.value()));
     }
 }
-
-/*
-HUNG : DONT REMOVE
-Here’s the table for your roleGroupRole.js mapping JS methods to controller APIs, in the format you requested:
-
-js method name	js api call	controller api	do we need to update js api call	suggestion
-loadRoleGroupRoles	${API_ROLE_GROUP_ROLE}/searchMappings?${qs}	/management/rolegrouprolemappings/searchMappings	No	Already correct
-addRoleToGroup	N/A	N/A	N/A	Navigation to addRoleGroupRole.jsp, no API call
-editRoleGroupRole	N/A	N/A	N/A	Navigation to editRoleGroupRole.jsp, no API call
-deleteRoleGroupRole	${API_ROLE_GROUP_ROLE}/deleteById?id=${id}	/management/rolegrouprolemappings/deleteById	No	Already correct
-saveRoleGroupRole	${API_ROLE_GROUP_ROLE}/add (create)
-${API_ROLE_GROUP_ROLE}/addIfNotExists?roleGroupId=...&roleId=... (edit)	/management/rolegrouprolemappings/add
-/management/rolegrouprolemappings/addIfNotExists	No	Already correct
-loadRoleGroupRoleById	${API_ROLE_GROUP_ROLE}/listAll	/management/rolegrouprolemappings/listAll	No	Already correct
-searchRoleGroupRoles	${API_ROLE_GROUP_ROLE}/searchMappings?${qs}	/management/rolegrouprolemappings/searchMappings	No	Already
-*/
