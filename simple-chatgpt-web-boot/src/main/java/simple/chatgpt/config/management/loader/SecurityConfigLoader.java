79d7412eb2b2d3aa5eced7014a45f87ffd2727a4
Hung En Tsai <ht02135@yahoo.com>
2025-10-10 17:02:46 -0400

    commit

--- simple-chatgpt-web-boot/src/main/java/simple/chatgpt/config/management/loader/SecurityConfigLoader.java
+++ simple-chatgpt-web-boot/src/main/java/simple/chatgpt/config/management/loader/SecurityConfigLoader.java
@@ -34,7 +34,7 @@ public class SecurityConfigLoader {
 
     @PostConstruct
     private void init() {
-        logger.debug("SecurityConfigLoader: init called");
+        logger.debug("init called");
 
         try (InputStream inputStream = SecurityConfigLoader.class.getResourceAsStream(DEFAULT_CONFIG_FILE)) {
             if (inputStream == null) {
@@ -45,120 +45,125 @@ public class SecurityConfigLoader {
                     .newDocumentBuilder()
                     .parse(inputStream);
 
-            // ===== Load roles =====
+            // =========================
+            // Load Roles
+            // =========================
             logger.debug("init Load roles ##############");
             NodeList roleNodes = document.getElementsByTagName("role");
             for (int i = 0; i < roleNodes.getLength(); i++) {
                 Element e = (Element) roleNodes.item(i);
                 String name = e.getAttribute("name");
                 String desc = e.getAttribute("description");
+
                 RoleConfig role = new RoleConfig(name, desc);
                 roles.add(role);
+
                 logger.debug("Loaded role name={}", name);
                 logger.debug("Loaded role description={}", desc);
             }
-            logger.debug("init Load roles ##############");
+            logger.debug("init Load roles DONE ##############");
 
-            // ===== Load role-groups =====
+            // =========================
+            // Load Role Groups
+            // =========================
             logger.debug("init Load role-groups ##############");
             NodeList groupNodes = document.getElementsByTagName("role-group");
             for (int i = 0; i < groupNodes.getLength(); i++) {
                 Element g = (Element) groupNodes.item(i);
                 String groupName = g.getAttribute("name");
+
                 RoleGroupConfig rg = new RoleGroupConfig(groupName, null);
 
                 NodeList roleRefs = g.getElementsByTagName("role-ref");
                 for (int j = 0; j < roleRefs.getLength(); j++) {
                     Element r = (Element) roleRefs.item(j);
-                    rg.addRole(new RoleRefConfig(r.getAttribute("name")));
+                    RoleRefConfig ref = new RoleRefConfig(r.getAttribute("name"));
+                    rg.addRole(ref);
+                    logger.debug("Added role-ref name={}", r.getAttribute("name"));
                 }
 
                 roleGroups.add(rg);
                 logger.debug("Loaded role group name={}", groupName);
                 logger.debug("Role group roles size={}", rg.getRoles().size());
             }
-            logger.debug("init Load role-groups ##############");
+            logger.debug("init Load role-groups DONE ##############");
 
-            // ===== Load page-role-groups =====
+            // =========================
+            // Load Page Role Groups
+            // =========================
             logger.debug("init Load page-role-groups ##############");
             NodeList pageNodes = document.getElementsByTagName("page-role-group");
             for (int i = 0; i < pageNodes.getLength(); i++) {
                 Element p = (Element) pageNodes.item(i);
                 String urlPattern = p.getAttribute("url-pattern");
-                String group = p.getAttribute("role-group");
-                PageRoleGroupConfig prg = new PageRoleGroupConfig(urlPattern, group);
+                String groupRef = p.getAttribute("role-group-ref"); // <-- use -ref
+                PageRoleGroupConfig prg = new PageRoleGroupConfig(urlPattern, groupRef);
                 pageRoleGroups.add(prg);
-                logger.debug("Loaded page-role-group urlPattern={}", urlPattern);
-                logger.debug("Loaded page-role-group roleGroup={}", group);
-            }
-            logger.debug("init Load page-role-groups ##############");
 
-            // ===== Load users =====
+                logger.debug("Loaded page-role-group urlPattern={}", urlPattern);
+                logger.debug("Loaded page-role-group roleGroupRef={}", groupRef);
+            }
+            logger.debug("init Load page-role-groups DONE ##############");
+
+            // =========================
+            // Load Users
+            // =========================
             logger.debug("init Load users ##############");
             NodeList userNodes = document.getElementsByTagName("user");
             for (int i = 0; i < userNodes.getLength(); i++) {
                 Element u = (Element) userNodes.item(i);
+                String roleGroupRef = u.getAttribute("role-group-ref"); // <-- use -ref
 
                 UserConfig user = new UserConfig(
-                    u.getAttribute("user_name"),
-                    u.getAttribute("user_key"),
-                    u.getAttribute("password"),
-                    u.getAttribute("first_name"),
-                    u.getAttribute("last_name"),
-                    u.getAttribute("email"),
-                    u.getAttribute("address_line_1"),
-                    u.getAttribute("address_line_2"),
-                    u.getAttribute("city"),
-                    u.getAttribute("state"),
-                    u.getAttribute("post_code"),
-                    u.getAttribute("country"),
-                    Boolean.parseBoolean(u.getAttribute("active")),
-                    Boolean.parseBoolean(u.getAttribute("locked")),
-                    u.getAttribute("role-group")
+                        u.getAttribute("user_name"),
+                        u.getAttribute("user_key"),
+                        u.getAttribute("password"),
+                        u.getAttribute("first_name"),
+                        u.getAttribute("last_name"),
+                        u.getAttribute("email"),
+                        u.getAttribute("address_line_1"),
+                        u.getAttribute("address_line_2"),
+                        u.getAttribute("city"),
+                        u.getAttribute("state"),
+                        u.getAttribute("post_code"),
+                        u.getAttribute("country"),
+                        Boolean.parseBoolean(u.getAttribute("active")),
+                        Boolean.parseBoolean(u.getAttribute("locked")),
+                        roleGroupRef
                 );
 
                 users.add(user);
                 logger.debug("Loaded user userName={}", user.getUserName());
-                logger.debug("Loaded user roleGroup={}", user.getRoleGroup());
+                logger.debug("Loaded user roleGroupRef={}", roleGroupRef);
             }
-            logger.debug("init Load users ##############");
+            logger.debug("init Load users DONE ##############");
 
         } catch (Exception e) {
             logger.error("Failed to load security configuration", e);
             throw new RuntimeException(e);
         }
-        
-        logger.debug("init ##############");
+
         logger.debug("init DONE!!!");
-        logger.debug("init ##############");
     }
 
     // ---------------- GETTERS ----------------
     public List<RoleConfig> getRoles() {
-    	logger.debug("getRoles called ##############");
-    	logger.debug("getRoles called roles={}", roles);
-    	logger.debug("getRoles called ##############");
+        logger.debug("getRoles called roles={}", roles);
         return roles;
     }
 
     public List<RoleGroupConfig> getRoleGroups() {
-    	logger.debug("getRoleGroups called ##############");
-    	logger.debug("getRoleGroups called roleGroups={}", roleGroups);
-    	logger.debug("getRoleGroups called ##############");
+        logger.debug("getRoleGroups called roleGroups={}", roleGroups);
         return roleGroups;
     }
 
     public List<PageRoleGroupConfig> getPageRoleGroups() {
-    	logger.debug("getPageRoleGroups called ##############");
-    	logger.debug("getPageRoleGroups called pageRoleGroups={}", pageRoleGroups);
-    	logger.debug("getPageRoleGroups called ##############");
+        logger.debug("getPageRoleGroups called pageRoleGroups={}", pageRoleGroups);
         return pageRoleGroups;
     }
 
     public List<UserConfig> getUsers() {
-    	logger.debug("getUsers called ##############");
-    	logger.debug("getUsers called users={}", users);
-    	logger.debug("getUsers called ##############");
+        logger.debug("getUsers called users={}", users);
         return users;
     }
 }
