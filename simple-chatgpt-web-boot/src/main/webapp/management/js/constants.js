740b60c5efa6fdf9f18f539ab5c70559714b2029
Hung En Tsai <ht02135@yahoo.com>
2025-10-14 11:38:09 -0400

    commit

--- simple-chatgpt-web-boot/src/main/webapp/management/js/constants.js
+++ simple-chatgpt-web-boot/src/main/webapp/management/js/constants.js
@@ -1,25 +1,61 @@
-
 // JSP
 const JSP_CONTEXT_PATH = window.location.origin + "/" + window.location.pathname.split("/")[1];
+console.debug("JSP_CONTEXT_PATH =", JSP_CONTEXT_PATH);
+
 const KO_SCRIPT = JSP_CONTEXT_PATH + "/management/js/knockout-latest.js";
+console.debug("KO_SCRIPT =", KO_SCRIPT);
+
 const LOGIN_PAGE = JSP_CONTEXT_PATH + "/management/jsp/auth/login.jsp";
+console.debug("LOGIN_PAGE =", LOGIN_PAGE);
+
 const LOGOUT_PAGE = JSP_CONTEXT_PATH + "/management/jsp/auth/logout.jsp";
+console.debug("LOGOUT_PAGE =", LOGOUT_PAGE);
+
 const REGISTER_PAGE = JSP_CONTEXT_PATH + "/management/jsp/auth/register.jsp";
+console.debug("REGISTER_PAGE =", REGISTER_PAGE);
+
 const INDEX_PAGE = JSP_CONTEXT_PATH + "/index.jsp";
+console.debug("INDEX_PAGE =", INDEX_PAGE);
+
 const DASHBOARD_PAGE = JSP_CONTEXT_PATH + "/dashboard.jsp";
+console.debug("DASHBOARD_PAGE =", DASHBOARD_PAGE);
+
 const API_AUTH_LOGIN = JSP_CONTEXT_PATH + "/api/management/auth/login";
+console.debug("API_AUTH_LOGIN =", API_AUTH_LOGIN);
+
 const API_AUTH_REGISTER = JSP_CONTEXT_PATH + "/api/management/auth/register";
+console.debug("API_AUTH_REGISTER =", API_AUTH_REGISTER);
 
 // API
 const API_CONTEXT_PATH = "/" + window.location.pathname.split("/")[1];
-const API_CONFIG = API_CONTEXT_PATH + "/api/management/config";
-const API_PROPERTY = API_CONTEXT_PATH + "/api/management/properties";
-const API_USER = API_CONTEXT_PATH + "/api/management/users";
-const API_USERLIST = API_CONTEXT_PATH + "/api/management/userlists";
-const API_USERLIST_MEMBER = API_CONTEXT_PATH + "/api/management/userlistmembers";
-const API_ROLE = API_CONTEXT_PATH + "/api/management/roles";
-const API_ROLE_GROUP = API_CONTEXT_PATH + "/api/management/rolegroups";
-const API_ROLE_GROUP_ROLE = API_CONTEXT_PATH + "/api/management/rolegrouprolemappings";
-const API_PAGE_ROLE_GROUP = API_CONTEXT_PATH + "/api/management/pagerolegroups";
-const API_USER_ROLE_GROUP = API_CONTEXT_PATH + "/api/management/userrolegroups";
+console.debug("API_CONTEXT_PATH =", API_CONTEXT_PATH);
 
+const API_CONFIG = API_CONTEXT_PATH + "/api/management/config";
+console.debug("API_CONFIG =", API_CONFIG);
+
+const API_PROPERTY = API_CONTEXT_PATH + "/api/management/properties";
+console.debug("API_PROPERTY =", API_PROPERTY);
+
+const API_USER = API_CONTEXT_PATH + "/api/management/users";
+console.debug("API_USER =", API_USER);
+
+const API_USERLIST = API_CONTEXT_PATH + "/api/management/userlists";
+console.debug("API_USERLIST =", API_USERLIST);
+
+const API_USERLIST_MEMBER = API_CONTEXT_PATH + "/api/management/userlistmembers";
+console.debug("API_USERLIST_MEMBER =", API_USERLIST_MEMBER);
+
+const API_ROLE = API_CONTEXT_PATH + "/api/management/roles";
+console.debug("API_ROLE =", API_ROLE);
+
+const API_ROLE_GROUP = API_CONTEXT_PATH + "/api/management/rolegroups";
+console.debug("API_ROLE_GROUP =", API_ROLE_GROUP);
+
+const API_ROLE_GROUP_ROLE = API_CONTEXT_PATH + "/api/management/rolegrouprolemappings";
+console.debug("API_ROLE_GROUP_ROLE =", API_ROLE_GROUP_ROLE);
+
+const API_PAGE_ROLE_GROUP = API_CONTEXT_PATH + "/api/management/pagerolegroups";
+console.debug("API_PAGE_ROLE_GROUP =", API_PAGE_ROLE_GROUP);
+
+const API_USER_ROLE_GROUP = API_CONTEXT_PATH + "/api/management/userrolegroups";
+console.debug("API_USER_ROLE_GROUP =", API_USER_ROLE_GROUP);
