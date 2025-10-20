c9910deebe4b5f9885f3c8da475508e9e7d7abe1
Hung En Tsai <ht02135@yahoo.com>
2025-10-20 16:00:34 -0400

    commit

--- simple-chatgpt-web-boot/src/main/java/simple/chatgpt/pojo/openai/crewai2/SequentialCrewExecutor.java
+++ simple-chatgpt-web-boot/src/main/java/simple/chatgpt/pojo/openai/crewai2/SequentialCrewExecutor.java
@@ -1,44 +1,35 @@
-package simple.chatgpt.service.openai.crewai2;
+package simple.chatgpt.pojo.openai.crewai2;
 
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import simple.chatgpt.pojo.openai.crewai2.Agent;
-import simple.chatgpt.pojo.openai.crewai2.Task;
-
 /*
  hung: sequential executor - runs tasks in order
  */
-public class SequentialCrewServiceImpl implements SequentialCrewService {
-    private static final Logger logger = LogManager.getLogger(SequentialCrewServiceImpl.class);
+public class SequentialCrewExecutor {
+    private static final Logger logger = LogManager.getLogger(SequentialCrewExecutor.class);
 
     private List<Task> tasks;
 
-    public SequentialCrewServiceImpl(List<Task> tasks) {
-        logger.debug("SequentialCrewServiceImpl constructor called");
-        logger.debug("SequentialCrewServiceImpl tasks={}", tasks);
-
+    public SequentialCrewExecutor(List<Task> tasks) {
+        logger.debug("SequentialCrewExecutor constructor called");
+        logger.debug("SequentialCrewExecutor tasks={}", tasks);
         this.tasks = tasks;
-        logger.debug("SequentialCrewServiceImpl this={}", this);
+        logger.debug("SequentialCrewExecutor this={}", this);
     }
 
     private static void init() {
         logger.debug("init called");
     }
 
-    @Override
     public void setTasks(List<Task> tasks) {
         logger.debug("setTasks called");
         logger.debug("setTasks tasks={}", tasks);
         this.tasks = tasks;
     }
 
-    /**
-     * Execute tasks in sequence. Each task's output is fed as input to the next task.
-     */
-    @Override
     public void execute(String initialInput) {
         logger.debug("execute called");
         logger.debug("execute initialInput={}", initialInput);
