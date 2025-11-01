c04aaa288249642a0178448ac06909e60d5ceb8a
Hung En Tsai <ht02135@yahoo.com>
2025-11-01 11:37:34 -0400

    commit

--- simple-python-tutorial/src/main/lesson1/hello.py
+++ simple-python-tutorial/src/main/lesson1/hello.py
@@ -1,5 +1,42 @@
 
 # hello.py
 # Run As → Python Run
+# python myfile.py 
 # https://www.w3schools.com/python/default.asp
+
+# hello
 print("Hello, World!")
+
+# Python Indentation
+if 5 > 2:
+  print("Five is greater than two!")
+  
+# Python Variables
+x = 5
+y = "Hello, World!"
+print(x)
+print(y)
+
+# Casting
+x2 = str(3)    # x will be '3'
+y2 = int(3)    # y will be 3
+z2 = float(3)  # z will be 3.0 
+print(x2)
+print(y2)
+print(z2)
+
+# Get the Type
+x3 = 5
+y3 = "John"
+print(type(x3))
+print(type(y3)) 
+
+# Single or Double Quotes?
+x4 = "John"
+# is the same as
+y4 = 'John'
+print(x4)
+print(y4)
+
+
+
---
+++ simple-python-tutorial/src/main/lesson1/python_numbers.py
@@ -0,0 +1,59 @@
+
+import random
+
+# Python Numbers
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Int
+x2 = 1
+y2 = 35656222554887711
+z2 = -3255522
+print(type(x2))
+print(type(y2))
+print(type(z2)) 
+
+# Float
+x3 = 1.10
+y3 = 1.0
+z3 = -35.59
+
+print(type(x3))
+print(type(y3))
+print(type(z3)) 
+
+#Type Conversion
+x4 = 1    # int
+y4 = 2.8  # float
+z4 = 1j   # complex
+
+#convert from int to float:
+a = float(x4)
+
+#convert from float to int:
+b = int(y4)
+
+#convert from int to complex:
+c = complex(x4)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c)) 
+
+print(random.randrange(1, 10)) 
+print(random.randrange(1, 10)) 
+print(random.randrange(1, 10))
+print(random.randrange(1, 10)) 
+
